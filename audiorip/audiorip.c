e5d5b9970aa85fe45342d7afdd4b477a87181ab2
Dave Brown <d.brown@bigdavedev.com>
2016-05-10 22:24:39 +0200

    Ascertain track length in frames
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

--- audiorip/audiorip.c
+++ audiorip/audiorip.c
@@ -5,6 +5,7 @@
 #include <linux/cdrom.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 
 /**
@@ -36,12 +37,22 @@ static struct argp argp = { options, parse_opt, NULL, doc };
  * end argp section
  */
 
+struct track_address
+{
+    int start;
+    int end;
+};
+
 static int cdrom_fd = -1;
 
 static void sigint_handler(int sig);
 static void spindown_and_close(int fd);
 
 static int get_num_tracks(int fd, int verbose);
+static int get_track_addresses(int fd,
+                               struct track_address* addresses,
+                               int num_tracks,
+                               int verbose);
 
 int main(int argc, char* argv[])
 {
@@ -66,6 +77,15 @@ int main(int argc, char* argv[])
     /* Get table of contents header to determine number of tracks */
     int num_tracks = get_num_tracks(cdrom_fd, arguments.verbose);
 
+    struct track_address *addresses = (struct track_address*)malloc(num_tracks * sizeof(struct track_address));
+    if (get_track_addresses(cdrom_fd, addresses, num_tracks, arguments.verbose) < 0)
+    {
+        free(addresses);
+        spindown_and_close(cdrom_fd);
+        return -1;
+    }
+
+    free(addresses);
     ioctl(cdrom_fd, CDROMSTOP);
     close(cdrom_fd);
     return 0;
@@ -121,4 +141,58 @@ static int get_num_tracks(int fd, int verbose)
     }
 
     return toc_header.cdth_trk1;
+}
+
+static int get_track_addresses(int fd,
+                               struct track_address* addresses,
+                               int num_tracks,
+                               int verbose)
+{
+    for (int i = 1; i <= num_tracks; ++i)
+    {
+        struct cdrom_tocentry current_track =
+        {
+            .cdte_track = i,
+            .cdte_format = CDROM_MSF
+        };
+
+        struct cdrom_tocentry next_track =
+        {
+            .cdte_track = i == num_tracks ? CDROM_LEADOUT : i+1,
+            .cdte_format = CDROM_MSF
+        };
+
+        if (ioctl(fd, CDROMREADTOCENTRY, &current_track) < 0)
+        {
+            fprintf(stderr, "Failed to read ToC entry for track: %d\n", i);
+            return -1;
+        }
+
+        if (ioctl(fd, CDROMREADTOCENTRY, &next_track) < 0)
+        {
+            if (next_track.cdte_track == CDROM_LEADOUT)
+            {
+                fprintf(stderr, "Failed to read ToC entry for leadout track\n");
+            }
+            else
+            {
+                fprintf(stderr, "Failed to read ToC entry for track: %d\n", i+1);
+            }
+            return -1;
+        }
+
+        addresses[i].start = current_track.cdte_addr.msf.frame
+                           + (current_track.cdte_addr.msf.minute * CD_FRAMES * 3600)
+                           + (current_track.cdte_addr.msf.second * CD_FRAMES)
+                           - CD_MSF_OFFSET;
+        addresses[i].end = next_track.cdte_addr.msf.frame
+                         + (next_track.cdte_addr.msf.minute * CD_FRAMES * 3600)
+                         + (next_track.cdte_addr.msf.second * CD_FRAMES)
+                         - CD_MSF_OFFSET;
+        if (verbose)
+        {
+            fprintf(stdout, "Track %d is %d frames long\n", i, addresses[i].end - addresses[i].start);
+        }
+    }
+    return 0;
 }
